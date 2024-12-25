3d11b30ea6c8381cbd59af5308a77457c496bf38
sokny2023 <psnpsn667788@gmail.com>
2024-12-25 20:36:17 +0700

    update get product details

--- src/main/java/devkh/asia/store_api/features/product/dto/ProductResponseDetails.java
+++ src/main/java/devkh/asia/store_api/features/product/dto/ProductResponseDetails.java
@@ -1,10 +1,15 @@
 package devkh.asia.store_api.features.product.dto;
 
+import devkh.asia.store_api.domain.User;
+
+import java.util.List;
+
 public record ProductResponseDetails(
         String name,
         String uuid,
         Integer quantity,
         Double price,
-        Boolean isDeleted
+        Boolean isDeleted,
+        List<User> users
 ) {
 }
